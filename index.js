7bd8e7ad36bd9b0390a98ae6f3976cee5e209de7
Joshua Palces <96431285+jpalces78@users.noreply.github.com>
2023-11-21 13:58:12 -0600

    commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/weather', async (req, res) => {
+  try {
+    const { city } = req.query;
+
+    if (!city) {
+      return res.status(400).json({ error: 'City parameter is required' });
+    }
+
+    const apiKey = '096d308b5b3e540b96398cc4d970b8b1';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await axios.get(apiUrl);
+
+    const weatherData = response.data;
+    res.json(weatherData);
+  } catch (error) {
+    console.error('Error fetching weather data:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
